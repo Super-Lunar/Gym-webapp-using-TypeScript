e74c98b9a12ca8753d6c0a1b65df7a975d95d2ee
sharmasaurabh4476@gmail.com <sharmasaurabh4476@gmail.com>
2024-11-01 08:10:46 +0545

    using react-anchor in navbar and adding links

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,10 +1,15 @@
-import { useState } from "react"
+
 import { Bars3Icon,XMarkIcon } from "@heroicons/react/24/solid"
 import Logo from "@/assets/Logo.png"
+import Link from "./Link"
 
-type Props = {}
+type Props = {
+     selectedPage:string;
+    setSelectedPage: (value: string) => void;
+}
 
-const Navbar = (props: Props) => {
+const Navbar = ({selectedPage,setSelectedPage}: Props) => {
+
     const flexBetween = "flex item-center justify-between"
   return (
     <nav>
@@ -19,9 +24,17 @@ const Navbar = (props: Props) => {
 <div className={`${flexBetween} w-full`}>
     {/* inner left side */}
     <div className={`${flexBetween} gap-8 text-sm`}>
-<p>Home</p>
-<p>About</p>
-<p>Contact</p>
+<Link 
+page="Home" 
+selectedPage={selectedPage}
+setSelectedPage={setSelectedPage}
+/>
+<Link page="Benefits" selectedPage={selectedPage}
+setSelectedPage={setSelectedPage}/>
+<Link page="Classes" selectedPage={selectedPage}
+setSelectedPage={setSelectedPage}/>
+<Link page="Contact Us" selectedPage={selectedPage}
+setSelectedPage={setSelectedPage}/>
     </div>
 
     {/* inner right side */}
