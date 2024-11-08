90f699a07d0a71e0872d49c4fef6ee0bb6e1e3f5
sharmasaurabh4476@gmail.com <sharmasaurabh4476@gmail.com>
2024-11-08 14:16:10 +0545

    Adding animation to benefit card

--- src/scenes/benefits/BenefitCard.tsx
+++ src/scenes/benefits/BenefitCard.tsx
@@ -1,7 +1,13 @@
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import { motion } from "framer-motion";
 //
 
 import { SelectedPage } from "@/shared/types";
+
+const childVariant = {
+  hidden: { opacity: 0, scale: 0.9 },
+  visible:{opacity:1,scale:1}
+}
 
 type Props = {
   icon: JSX.Element;
@@ -12,7 +18,9 @@ type Props = {
 
 const BenefitCard = ({ icon, title, description, setSelectedPage }: Props) => {
   return (
-    <div className="mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center">
+    <motion.div className="mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center "
+    
+    variants={childVariant}>
       <div className="mb-4 flex justify-center">
         <div className="rounded-full border-2 border-gray-100 bg-primary-100 p-4">
           {icon}
@@ -29,7 +37,7 @@ const BenefitCard = ({ icon, title, description, setSelectedPage }: Props) => {
       >
         <p>Learn More</p>
       </AnchorLink>
-    </div>
+    </motion.div>
   );
 };
 
