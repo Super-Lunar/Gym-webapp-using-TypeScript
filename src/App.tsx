bd6381ddfa61961e7e4e2185777a45719605fd24
sharmasaurabh4476@gmail.com <sharmasaurabh4476@gmail.com>
2024-11-08 20:51:55 +0545

    our classes page created and benefits card modified with read more ... added

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { SelectedPage } from "@/shared/types";
 import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home";
 import Benefits from "@/scenes/benefits";
+import OurClasses from "@/scenes/ourclasses";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -35,6 +36,7 @@ function App() {
       />
       <Home setSelectedPage={setSelectedPage} />
       <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
     </div>
   );
 }
--- src/scenes/benefits/BenefitCard.tsx
+++ src/scenes/benefits/BenefitCard.tsx
@@ -1,13 +1,14 @@
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import { motion } from "framer-motion";
+import { useState } from "react";
 //
 
 import { SelectedPage } from "@/shared/types";
 
 const childVariant = {
   hidden: { opacity: 0, scale: 0.9 },
-  visible:{opacity:1,scale:1}
-}
+  visible: { opacity: 1, scale: 1 },
+};
 
 type Props = {
   icon: JSX.Element;
@@ -17,17 +18,31 @@ type Props = {
 };
 
 const BenefitCard = ({ icon, title, description, setSelectedPage }: Props) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleDescription = () => {
+    setIsExpanded(!isExpanded);
+  };
   return (
-    <motion.div className="mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center  "
-    
-    variants={childVariant}>
+    <motion.div
+      className="mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center"
+      variants={childVariant}
+    >
       <div className="mb-4 flex justify-center">
         <div className="rounded-full border-2 border-gray-100 bg-primary-100 p-4">
           {icon}
         </div>
       </div>
       <h4 className="font-bold">{title}</h4>
-      <p className="my-3">{description}</p>
+      <p className="my-3">
+        {isExpanded ? description : `${description.slice(0, 80)}...`}
+        <button
+          onClick={toggleDescription}
+          className="ml-2 font-semibold text-primary-500 hover:underline"
+        >
+          {isExpanded ? "Show less" : "Read more"}
+        </button>
+      </p>
       <AnchorLink
         className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
         onClick={() => {
